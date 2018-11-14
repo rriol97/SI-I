bf8d4f252a72dfb73f8423e5d61612510724fcc6
Ricardo <ricardo.riol@estudiante.uam.es>
2018-11-14 01:18:06 +0100

    vamos

--- p3/actualiza.sql
+++ p3/actualiza.sql
@@ -16,3 +16,22 @@ ALTER TABLE imdb_actormovies ADD FOREIGN KEY (actorid) REFERENCES imdb_actors(ac
 
 --Anadimos foreing key prod_id a la tabla inventory
 ALTER TABLE inventory ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+
+--Anadimos los campos de stock y sales a products
+ALTER TABLE products ADD COLUMN stock integer default 0;
+ALTER TABLE products ALTER COLUMN stock SET NOT NULL;
+ALTER TABLE products ADD COLUMN sales integer default 0;
+ALTER TABLE products ALTER COLUMN sales SET NOT NULL;
+
+-- Inicializamos los valores de ambas columnas
+INSERT INTO products (prod_id, movieid, price, description, stock, sales)
+	SELECT products.prod_id, products.movieid, products.price, products.description, inventory.stock, inventory.sales 
+	FROM inventory, products 
+	WHERE products.prod_id = inventory.prod_id;
+
+
+UPDATE products
+SET products.stock = inventory.stock
+FROM products INNER JOIN inventory
+ON inventory.prod_id=products.prod_id;
+
