5c72e60b0fb111b9f3e1a68fdef37f9327bf357a
emiliocuestaf <emiliocuestaf@gmail.com>
2018-11-14 02:16:31 +0100

    actualizacion lista

--- p3/actualiza.sql
+++ p3/actualiza.sql
@@ -15,16 +15,9 @@ ALTER TABLE imdb_actormovies ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(mo
 ALTER TABLE imdb_actormovies ADD FOREIGN KEY (actorid) REFERENCES imdb_actors(actorid);
 
 --Inicializamos los valores de ambas columnas
-SELECT products.prod_id, products.movieid, products.price, products.description, inventory.stock, inventory.sales
+SELECT products.prod_id, products.movieid, products.price, products.description, CASE WHEN inventory.prod_id is NULL THEN 0 ELSE inventory.stock END AS stock , CASE WHEN inventory.prod_id is NULL THEN 0 ELSE inventory.sales END AS sales
 INTO productsAUX
-FROM products inner join inventory
-
-on products.prod_id = inventory.prod_id;
-
--- Clave primaria de products
-ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (prod_id);
--- Clave foranea de movieid
-ALTER TABLE products ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid);
+FROM products LEFT JOIN inventory ON products.prod_id = inventory.prod_id;
 
 --Borramos las tablas inventory y products
 DROP TABLE products;
@@ -32,6 +25,11 @@ DROP TABLE inventory;
 
 --Ronombramos productsAUX a products
 ALTER TABLE productsAUX RENAME TO products;
+
+-- Clave primaria de products
+ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (prod_id);
+-- Clave foranea de movieid
+ALTER TABLE products ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid);
 
 --Anadimos clave primaria a orderdetail
 SELECT orderid, prod_id, sum(price) as price, sum(quantity) as quantity
@@ -47,7 +45,7 @@ ALTER TABLE orderdetail ADD CONSTRAINT orderdetail_pkey PRIMARY KEY (orderid,pro
 
 -- Anadimos claves foraneas a orderdetail
 ALTER TABLE orderdetail ADD FOREIGN KEY (orderid) REFERENCES orders(orderid);
---(no funciona: prod_id 3215 no existe) ALTER TABLE orderdetail ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+ALTER TABLE orderdetail ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
 
  
 
