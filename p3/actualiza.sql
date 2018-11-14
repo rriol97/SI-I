6c1565b29b0da635adef83f0be9a730a1a4ec06a
Ricardo <ricardo.riol@estudiante.uam.es>
2018-11-14 01:49:59 +0100

    Puto 3215

--- p3/actualiza.sql
+++ p3/actualiza.sql
@@ -14,24 +14,41 @@ ALTER TABLE imdb_directormovies ADD CONSTRAINT imdb_directormovies_pkey PRIMARY 
 ALTER TABLE imdb_actormovies ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid);
 ALTER TABLE imdb_actormovies ADD FOREIGN KEY (actorid) REFERENCES imdb_actors(actorid);
 
---Anadimos foreing key prod_id a la tabla inventory
-ALTER TABLE inventory ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+--Inicializamos los valores de ambas columnas
+SELECT products.prod_id, products.movieid, products.price, products.description, inventory.stock, inventory.sales
+INTO productsAUX
+FROM products inner join inventory
 
---Anadimos los campos de stock y sales a products
-ALTER TABLE products ADD COLUMN stock integer default 0;
-ALTER TABLE products ALTER COLUMN stock SET NOT NULL;
-ALTER TABLE products ADD COLUMN sales integer default 0;
-ALTER TABLE products ALTER COLUMN sales SET NOT NULL;
+on products.prod_id = inventory.prod_id;
 
--- Inicializamos los valores de ambas columnas
-INSERT INTO products (prod_id, movieid, price, description, stock, sales)
-	SELECT products.prod_id, products.movieid, products.price, products.description, inventory.stock, inventory.sales 
-	FROM inventory, products 
-	WHERE products.prod_id = inventory.prod_id;
+-- Clave primaria de products
+ALTER TABLE products ADD CONSTRAINT products_pkey PRIMARY KEY (prod_id);
+-- Clave foranea de movieid
+ALTER TABLE products ADD FOREIGN KEY (movieid) REFERENCES imdb_movies(movieid);
 
+--Borramos las tablas inventory y products
+DROP TABLE products;
+DROP TABLE inventory;
 
-UPDATE products
-SET products.stock = inventory.stock
-FROM products INNER JOIN inventory
-ON inventory.prod_id=products.prod_id;
+--Ronombramos productsAUX a products
+ALTER TABLE productsAUX RENAME TO products;
+
+--Anadimos clave primaria a orderdetail
+SELECT orderid, prod_id, sum(price) as price, sum(quantity) as quantity
+INTO orderdetailAUX
+FROM orderdetail
+GROUP BY orderid, prod_id;
+
+DROP TABLE orderdetail;
+
+ALTER TABLE orderdetailAUX RENAME TO orderdetail;
+
+ALTER TABLE orderdetail ADD CONSTRAINT orderdetail_pkey PRIMARY KEY (orderid,prod_id);
+
+-- Anadimos claves foraneas a orderdetail
+ALTER TABLE orderdetail ADD FOREIGN KEY (orderid) REFERENCES orders(orderid);
+--(no funciona: prod_id 3215 no existe) ALTER TABLE orderdetail ADD FOREIGN KEY (prod_id) REFERENCES products(prod_id);
+
+ 
+
 
