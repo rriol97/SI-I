057f5a7331aa08ecd74dd6330f64fc6cefea3524
emiliocuestaf <emiliocuestaf@gmail.com>
2018-11-15 11:45:39 +0100

    apartado d listo

--- p3/setOrderAmount.sql
+++ p3/setOrderAmount.sql
@@ -1,6 +1,19 @@
-﻿UPDATE orders
-SET 
-	netamount = orderdetail.price * orderdetail.quantity,
-	totalamount = netamount*tax/100
-FROM orderdetail
-WHERE orders.orderid = orderdetail.orderid AND (netamount is NULL OR totalamount is NULL);
+DROP FUNCTION IF EXISTS setOrderAmount();
+
+CREATE OR REPLACE FUNCTION setOrderAmount()
+RETURNS void AS $$
+BEGIN
+	UPDATE orders
+	SET    netamount = ordernetprice.wholeorderprice,
+	       totalamount = netamount*(100+tax)/100
+	FROM (SELECT orderid, sum(price*quantity) as wholeorderprice
+	      FROM orderdetail NATURAL JOIN orders
+	      GROUP BY orderid) as ordernetprice
+	WHERE ordernetprice.orderid=orders.orderid AND (netamount is NULL OR totalamount is NULL);
+
+END; $$ 
+LANGUAGE plpgsql;
+
+-- Realmente da igual invocar esta funcion en select o en from puesto que no devuelve nada, solo actualiza la base de datos.
+SELECT * 
+FROM setOrderAmount();
--- p3/setPrice.sql
+++ p3/setPrice.sql
@@ -1,8 +1,11 @@
 --Actualizamos price de orderdetail
 
-update orderdetail
-set price = foo.price
-from (select orderdetail.orderid, orderdetail.prod_id, products.price / pow(1.02, extract (year from now()) - extract (year from orders.orderdate)) as price, orderdetail.quantity
-from orders, orderdetail, products
-where orderdetail.orderid = orders.orderid and orderdetail.prod_id = products.prod_id) as foo
-where orderdetail.orderid = foo.orderid and orderdetail.prod_id = foo.prod_id and orderdetail.quantity = foo.quantity;
+UPDATE orderdetail
+SET price = foo.price
+FROM (SELECT orderdetail.orderid, 
+	     orderdetail.prod_id, 
+	     products.price / pow(1.02, extract (year from now()) - extract (year from orders.orderdate)) as price, 
+             orderdetail.quantity
+      FROM orders, orderdetail, products
+      WHERE orderdetail.orderid = orders.orderid AND orderdetail.prod_id = products.prod_id) AS foo
+ WHERE orderdetail.orderid = foo.orderid AND orderdetail.prod_id = foo.prod_id AND orderdetail.quantity = foo.quantity;
