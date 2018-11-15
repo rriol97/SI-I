bc5f9a7b2b5c7e6b2e5154c6c1e03d78a343f317
emiliocuestaf <emiliocuestaf@gmail.com>
2018-11-15 02:13:03 +0100

    uploading f, d aun no funciona

---
+++ p3/getTopMonths.sql
@@ -0,0 +1,18 @@
+﻿DROP FUNCTION IF EXISTS gettopmonths(integer,integer);
+
+CREATE OR REPLACE FUNCTION getTopMonths(prodbound int4,totamountbound int4)
+RETURNS TABLE(year integer, month integer, amount numeric, numProducts numeric) AS $$
+BEGIN
+	RETURN QUERY 
+		SELECT cast(date_part('year',orderdate) as integer) as year, 
+		       cast(date_part('month',orderdate) as integer) as month,
+		       sum(totalamount) as amount,
+		       sum(quantity) as numProducts
+		FROM orders NATURAL JOIN orderdetail
+		GROUP BY year, month
+		HAVING sum(totalamount) > totamountbound  OR sum(quantity) > prodBound;
+END; $$ 
+LANGUAGE plpgsql;
+
+SELECT * 
+FROM getTopMonths(19000,320000);
---
+++ p3/setOrderAmount.sql
@@ -0,0 +1,6 @@
+﻿UPDATE orders
+SET 
+	netamount = orderdetail.price * orderdetail.quantity,
+	totalamount = netamount*tax/100
+FROM orderdetail
+WHERE orders.orderid = orderdetail.orderid AND (netamount is NULL OR totalamount is NULL);
